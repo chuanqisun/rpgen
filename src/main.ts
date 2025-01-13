5768bd00c832ec101567dcdff0ad60d9e736d29e
Chauqni Sun <chuanqi.sun.dev@gmail.com>
2025-01-12 17:45:43 -0800

    test azure stt

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { loadAIBar } from "./features/ai-bar/loader";
+import { AzureSttNode } from "./features/ai-bar/nodes/azure-stt-node";
 import type { AzureTtsNode } from "./features/ai-bar/nodes/azure-tts-node";
 import type { OpenAILlmNode } from "./features/ai-bar/nodes/openai-llm-node";
 import type { WebTtsNode } from "./features/ai-bar/nodes/web-tts-node";
+import { sttRecognizedEbentName, type SttRecognizedEventDetails } from "./features/ai-bar/shared/stt";
 import { user } from "./features/llm/message";
 import "./style.css";
 
@@ -25,3 +27,29 @@ document.querySelector<HTMLButtonElement>(`[data-action="test-web-tts"]`)?.addEv
   const tts = await document.querySelector<WebTtsNode>("web-tts-node");
   tts?.queue("Hello world", { onStart: () => console.log("start"), onEnd: () => console.log("end") });
 });
+
+document.querySelector<HTMLButtonElement>(`[data-action="test-azure-stt"]`)?.addEventListener(
+  "click",
+  async () => {
+    const stt = document.querySelector<AzureSttNode>("azure-stt-node")!;
+    await stt.startMicrophone();
+    const trigger = document.querySelector<HTMLButtonElement>(`[data-action="test-azure-stt"]`)!;
+    trigger.textContent = "Hold to speak";
+
+    trigger.addEventListener("mousedown", async () => {
+      trigger.textContent = "Release to send";
+      stt?.start();
+    });
+
+    trigger.addEventListener("mouseup", async () => {
+      trigger.textContent = "Hold to speak";
+      stt?.stop();
+    });
+
+    stt.addEventListener(sttRecognizedEbentName, (e) => {
+      const detail = (e as CustomEvent<SttRecognizedEventDetails>).detail;
+      console.log(detail);
+    });
+  },
+  { once: true }
+);
