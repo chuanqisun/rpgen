12eddf2d334f59b2bd12822b5c5b7a67142cfe6d
chuanqisun <chuanqi.sun.dev@gmail.com>
2025-01-30 17:32:02 -0800

    refactor library use

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import type { AzureTtsNode, OpenAILlmNode, SttRecognizedEventDetails, WebSttNode, WebTtsNode } from "natural-kit";
-import { AzureSttNode, loadAIBar, sttRecognizedEventName, user } from "natural-kit";
+import type { OpenAILlmNode } from "natural-kit";
+import { loadAIBar, user } from "natural-kit";
 import "./style.css";
 
 loadAIBar();
@@ -12,64 +12,3 @@ document.querySelector<HTMLButtonElement>(`[data-action="test-chat"]`)?.addEvent
   });
   console.log(response);
 });
-
-document.querySelector<HTMLButtonElement>(`[data-action="test-azure-tts"]`)?.addEventListener("click", async () => {
-  const tts = await document.querySelector<AzureTtsNode>("azure-tts-node");
-  tts?.queue("Hello world", { onStart: () => console.log("start"), onEnd: () => console.log("end") });
-});
-
-document.querySelector<HTMLButtonElement>(`[data-action="test-web-tts"]`)?.addEventListener("click", async () => {
-  const tts = await document.querySelector<WebTtsNode>("web-tts-node");
-  tts?.queue("Hello world", { onStart: () => console.log("start"), onEnd: () => console.log("end") });
-});
-
-document.querySelector<HTMLButtonElement>(`[data-action="test-azure-stt"]`)?.addEventListener(
-  "click",
-  async () => {
-    const stt = document.querySelector<AzureSttNode>("azure-stt-node")!;
-    await stt.startMicrophone();
-    const trigger = document.querySelector<HTMLButtonElement>(`[data-action="test-azure-stt"]`)!;
-    trigger.textContent = "Hold to speak";
-
-    trigger.addEventListener("mousedown", async () => {
-      trigger.textContent = "Release to send";
-      stt?.start();
-    });
-
-    trigger.addEventListener("mouseup", async () => {
-      trigger.textContent = "Hold to speak";
-      stt?.stop();
-    });
-
-    stt.addEventListener(sttRecognizedEventName, (e) => {
-      const detail = (e as CustomEvent<SttRecognizedEventDetails>).detail;
-      console.log(detail);
-    });
-  },
-  { once: true }
-);
-
-document.querySelector<HTMLButtonElement>(`[data-action="test-web-stt"]`)?.addEventListener(
-  "click",
-  async () => {
-    const stt = document.querySelector<WebSttNode>("web-stt-node")!;
-    const trigger = document.querySelector<HTMLButtonElement>(`[data-action="test-web-stt"]`)!;
-    trigger.textContent = "Hold to speak";
-
-    trigger.addEventListener("mousedown", async () => {
-      trigger.textContent = "Release to send";
-      stt?.start();
-    });
-
-    trigger.addEventListener("mouseup", async () => {
-      trigger.textContent = "Hold to speak";
-      stt?.stop();
-    });
-
-    stt.addEventListener(sttRecognizedEventName, (e) => {
-      const detail = (e as CustomEvent<SttRecognizedEventDetails>).detail;
-      console.log(detail);
-    });
-  },
-  { once: true }
-);
